b6bd09b2a4b374f40006d0a735d9183e37833264
tottipensotti <ag.pensotti@gmail.com>
2022-12-21 13:20:59 -0300

    added stats and re-factoring some functions

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+from game_stats import GameStats
 import game_functions as gf
 
 def run_game():
@@ -13,13 +14,15 @@ def run_game():
     ship = Ship(ai_settings, screen)
     bullets = Group()
     aliens = Group()
+    stats = GameStats(ai_settings)
     gf.create_fleet(ai_settings, screen, ship, aliens)
 
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
-        gf.update_aliens(ai_settings, aliens)
 
 run_game()
